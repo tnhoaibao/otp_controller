3339256f5bf382267b44fabf259160c72c91ff0f
shinichi <tnhoaibao@gmail.com>
2021-04-02 14:22:58 +0700

    version 2.0

--- model/efuse_model.sv
+++ model/efuse_model.sv
@@ -18,3 +18,33 @@ output [7:0] Q;
 input VDDQ;
 input [9:0] A;
 input PGENB;
+
+wire CSB_clk_n;
+
+// determine operation mode
+always @(*)
+  begin
+    if ((CSB == 1'b0) && (VDDQ == 1'b1) && (PGENB == 1'b0) && (LOAD == 1'b0)) begin
+	  PGM_MODE = 1'b1;
+	  READ_MODE = 1'b0;
+	else if ((CSB == 1'b0) && (VDDQ == 1'b0) && (PGENB == 1'b1) && (LOAD == 1'b1)) begin
+	  PGM_MODE = 1'b0;
+	  READ_MODE = 1'b1;
+	else 
+	  PGM_MODE = 1'b0;
+	  READ_MODE = 1'b0;
+  end
+
+assign rst_mode = PGM_MODE | READ_MODE;
+
+// implement fsm for otp memory
+// model TEF65GP128x8HD
+always @(posedge STROBE or negedge rst_mode)
+  begin
+    if (rst_mode == 1'b0)
+	  
+    if (
+	else 
+	  if (
+		
+  end
